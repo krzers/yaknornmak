114e6daaa72f6a4df469ab9f8b0d7fce8ab1567b
krzers <tyhffggfg@gmail.com>
2023-12-12 15:51:16 +0700

    jakidlakub

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -1,3 +1,55 @@
+#include <iostream>
+using namespace std;
+int agenumber,heightnumber,bounty;
+int main(){
+    cout << "Enter your age: ";
+    cin >> agenumber;
+    if(agenumber <= 25){
+        cout << "Enter your height: ";
+        cin >> heightnumber;
+        if(heightnumber < 100){
+            cout << "Your character = Chopper"; 
+        }
+        else if(heightnumber < 180){
+            cout << "Your character = Usopp"; 
+        }
+        else{
+            cout << "Enter your bounty: ";
+            cin >> bounty;
+            if (bounty > 1100000000){
+                cout << "Your character = Zoro";
+            }
+            else{
+                cout << "Your character = Sanji";
+            }
+        }
+    }
+    else if(agenumber <= 60){
+        cout << "Enter your bounty: ";
+        cin >> bounty;
+        if(bounty > 500000000){
+            cout << "Your character = Jinbe";
+        }
+        else{
+            cout << "Your character = Franky";
+        }
+    }
+    else{
+        cout << "Your character = Brook";
+    }
+}
+
+
+
+
+
+
+
+
+
+
+}
+
 /*
 "Enter your age: "
 "Enter your height: "
